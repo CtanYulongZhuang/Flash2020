98930cdddd8b6a074922539742d853ad5d84d8e8
Yulong Zhuang <ctanzhuangyl@gmail.com>
2021-03-10 14:46:09 +0100

    Fixing bugs

--- ellipsoid_fitting_funfit.py
+++ ellipsoid_fitting_funfit.py
@@ -8,7 +8,8 @@ from scipy.signal import savgol_filter
 from scipy.interpolate import interp1d
 import multiprocessing as mp
 import matplotlib.pyplot as plt
-#/asap3/flash/gpfs/bl1/2020/data/11007613/shared/aux/PL
+from scipy.optimize import curve_fit
+#/asap3/flash/gpfs/bl1/2020/data/11007613/shared/aux/
 
 
 import h5py
@@ -22,21 +23,40 @@ qz = data_tem['qz'][:]
 mask = data_tem['mask'][:]
 data_tem.close()
 
-def ellipse_r(a,b,t,tp):
-    t_all = t + tp
-    return  a*b/np.sqrt(a**2*np.sin(t_all)**2 + b**2*np.cos(t_all)**2)
+
+def intens(X, a, b, tp):
+    rx,tx = X
+    t_all = tx + tp
+    radii = a*b/np.sqrt(a**2*np.sin(t_all)**2 + b**2*np.cos(t_all)**2)
+    det_dist = 100
+    qval = 2. * np.sin(0.5 * np.arctan(rx * 0.075 / det_dist)) / 4.5
+    diameter = 2*radii
+    #svals = np.outer(diameter, qval) * np.pi
+    svals = diameter * qval * np.pi
+    #svals[svals==0] = 1e-6
+    sval = svals
+    intensity = (np.sin(sval) - sval * np.cos(sval))**2 / sval**6 * diameter**6
+    return intensity
 
 
-a =3
-b= 5
-t = np.arange(1000)*0.02
-tp = np.arange(60)/60*np.pi
-r = ellipse_r(a,b,t,tp[15])
-plt.scatter(r*np.sin(t), r*np.cos(t),s=1)
 
-for i in range(10):
-    r = ellipse_r(a,b,t,tp[i*3])
-    plt.scatter(r*np.sin(t), r*np.cos(t),s=1)
+#[247,255] Closest point to the center rmin = 67
+intrad0 = np.load('../aux/intrad_004.npy')
+intrad = intrad0[256:768,256:512]
+x0,y0 = np.indices((512,256)); x0-=247; y0-=(255+67)
+plt.scatter(x0,y0,c = intrad)
+floatang = np.arccos(x0/intrad)
+flat_floatang = floatang.ravel()
+flat_intrad = intrad.ravel()
+
+X = [flat_intrad, flat_floatang]
+intensity_map = intens(X, a, b, tp)
+plt.imshow(np.log10(intensity_map.reshape(512,256)))
+
+p0 = 28., 22., np.pi/2.
+Pa, Pb = curve_fit(intens, X, intensity_map, p0)
+
+
 
 
 #poisson
@@ -71,17 +91,10 @@ floatang = np.arccos(x0/intrad)
 divang = np.round(floatang, 1)
 divrad = intrad//2*2
 
-rad_pool = np.unique(divrad)
-ang_pool = np.unique(divang)
-for i in rad_pool:
-    for j in ang_pool:
-        ns = np.where((divrad == i) & (divang == j))[0]
-        intens =
-
-
-
-
 flotrad = intrad[mask_center]
+flotang = floatang[mask_center]
+x1 = x0[mask_center]
+y1 = y0[mask_center]
 
 s_s = np.load('../aux/int2std_model_r30_mask0_weighted.npz')
 std_models = s_s['std_model']
@@ -90,59 +103,45 @@ std_smooth = savgol_filter(std_models, 51, 3)
 fis = interp1d(np.log10(int_models), std_smooth,'cubic')
 
 
-radcount = np.zeros(intrad.max() + 1)
-np.add.at(radcount, intrad, mask_center)
-radmask = (radcount > 10)
-radcount[radcount == 0] = 1
 
-det_dist = 100 # mm
-qvals = 2. * np.sin(0.5 * np.arctan(np.arange(int(intrad.max())+1) * 0.075 / det_dist)) / 4.5
-diameters = np.linspace(10, 50, 1200)
-svals = np.outer(diameters, qvals) * np.pi
-svals[svals==0] = 1e-6
-spheremodels = (np.sin(svals) - svals * np.cos(svals))**2 / svals**6 * diameters[:,np.newaxis]**6
+X = [flotrad, flotang]
+
+p0 = 27., 27., np.pi/2.
+Pa, Pb = curve_fit(intens, X, data_i, p0)
+
 
 
 ###############################################
 intens_tot = mp.Array(ctypes.c_double, n_frames)
 cc = mp.Array(ctypes.c_double, n_frames)
-error = mp.Array(ctypes.c_double, n_frames)
-dia = mp.Array(ctypes.c_double, n_frames)
+angle = mp.Array(ctypes.c_double, n_frames)
+dia_a = mp.Array(ctypes.c_double, n_frames)
+dia_b = mp.Array(ctypes.c_double, n_frames)
 
-def mp_worker(rank, indices, dia, intens_tot, cc, error):
+def mp_worker(rank, indices, dia_a, dia_b, angle):
     irange = indices[rank::nproc]
 
     for i in irange:
         f_i = emc.get_frame(i)
         data_i = f_i.data[mask_center]
-        radavg = np.zeros_like(radcount)
-        tdata = data_i
-        np.add.at(radavg, flotrad, tdata)
-        radavg /= radcount
-
-        corrs = np.corrcoef(((radcount*radavg)[radmask]), (radcount*spheremodels)[:,radmask])[0, 1:]
-        cc[i] = corrs.max()
-        dia[i] = diameters[corrs.argmax()]
-        intens_tot[i] = np.sum(tdata)
-        if (intens_tot[i] >=10):
-            error[i] = fis(np.log10(intens_tot[i]))
-        if (intens_tot[i] < 10):
-            error[i] = 100
+        p0 = 27., 27., np.pi/2.
+        Pa, Pb = curve_fit(intens, X, data_i, p0)
+        angle[i] = Pa[2]
+        dia_a[i] = Pa[0]
+        dia_b[i] = Pa[1]
 
         if rank == 0:
-            print("CC (%d):"%i, ' intens = ', intens_tot[i], ' dia = ', dia[i], ' err = ', error[i])
-
+            print("CC (%d):"%i, ' sum_i = ', np.sum(data_i),  ' dia_a = ', dia_a[i], ' dia_b = ', dia_b[i], ' angle = ', angle[i])
 
 
 nproc = 16
 ind = np.arange(n_frames)
-jobs = [mp.Process(target=mp_worker, args=(rank, ind, dia, intens_tot, cc, error)) for rank in range(nproc)]
+jobs = [mp.Process(target=mp_worker, args=(rank, ind, dia_a, dia_b, angle)) for rank in range(nproc)]
 [i.start() for i in jobs]
 [i.join() for i in jobs]
-dia = np.frombuffer(dia.get_obj())
-intens_tot = np.frombuffer(intens_tot.get_obj())
-cc = np.frombuffer(cc.get_obj())
-error = np.frombuffer(error.get_obj())
+dia_a = np.frombuffer(dia_a.get_obj())
+dia_b = np.frombuffer(dia_b.get_obj())
+angle = np.frombuffer(angle.get_obj())
 ###############################################
 with h5py.File('diameter_weighted_error_mask0.h5', 'w') as f:
     f['Run_num'] = running_num
