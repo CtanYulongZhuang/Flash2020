0152d7b8f3678510bda270d78d7fcc2f11f3346b
Yulong Zhuang <ctanzhuangyl@gmail.com>
2021-03-11 10:53:22 +0100

    add scale in the fitting, separate major and minor axis

--- ellipsoid_fitting_funfit.py
+++ ellipsoid_fitting_funfit.py
@@ -24,7 +24,7 @@ mask = data_tem['mask'][:]
 data_tem.close()
 
 
-def intens(X, a, b, tp):
+def intens(X, a, b, tp, scl):
     rx,tx = X
     t_all = tx + tp
     radii = a*b/np.sqrt(a**2*np.sin(t_all)**2 + b**2*np.cos(t_all)**2)
@@ -35,9 +35,8 @@ def intens(X, a, b, tp):
     svals = diameter * qval * np.pi
     #svals[svals==0] = 1e-6
     sval = svals
-    intensity = (np.sin(sval) - sval * np.cos(sval))**2 / sval**6 * diameter**6
+    intensity = scl*(np.sin(sval) - sval * np.cos(sval))**2 / sval**6 * diameter**6
     return intensity
-
 
 
 #[247,255] Closest point to the center rmin = 67
@@ -48,16 +47,6 @@ plt.scatter(x0,y0,c = intrad)
 floatang = np.arccos(x0/intrad)
 flat_floatang = floatang.ravel()
 flat_intrad = intrad.ravel()
-
-X = [flat_intrad, flat_floatang]
-intensity_map = intens(X, a, b, tp)
-plt.imshow(np.log10(intensity_map.reshape(512,256)))
-
-p0 = 28., 22., np.pi/2.
-Pa, Pb = curve_fit(intens, X, intensity_map, p0)
-
-
-
 
 #poisson
 det = detector.Detector('../aux/det_intrad4.h5', mask_flag=True)
@@ -72,9 +61,6 @@ running_num = np.zeros(np.max(clist))
 running_num[0:clist[0]] = np.int( emc_flist[0][75:78] )
 for i in range(len(clist)-1):
     running_num[clist[i]:clist[i+1]] = np.int( emc_flist[i+1][75:78] )
-
-
-
 
 
 mask_emc0 = emc.get_frame(1).mask.ravel()
@@ -105,30 +91,33 @@ fis = interp1d(np.log10(int_models), std_smooth,'cubic')
 
 
 X = [flotrad, flotang]
-
-p0 = 27., 27., np.pi/2.
-Pa, Pb = curve_fit(intens, X, data_i, p0)
-
-
-
 ###############################################
 intens_tot = mp.Array(ctypes.c_double, n_frames)
 cc = mp.Array(ctypes.c_double, n_frames)
 angle = mp.Array(ctypes.c_double, n_frames)
 dia_a = mp.Array(ctypes.c_double, n_frames)
 dia_b = mp.Array(ctypes.c_double, n_frames)
+scale = mp.Array(ctypes.c_double, n_frames)
+sum_i = mp.Array(ctypes.c_double, n_frames)
 
-def mp_worker(rank, indices, dia_a, dia_b, angle):
+def mp_worker(rank, indices, dia_a, dia_b, angle, scale, sum_i):
     irange = indices[rank::nproc]
 
     for i in irange:
         f_i = emc.get_frame(i)
         data_i = f_i.data[mask_center]
-        p0 = 27., 27., np.pi/2.
-        Pa, Pb = curve_fit(intens, X, data_i, p0)
+        p0 = 17., 15., 0, 0.1
+        #Pa, Pb = curve_fit(intens, X, data_i, p0, maxfev=100000)
+        Pa = [0,0,0,0]
+        try:
+            Pa, Pb = curve_fit(intens, X, data_i, p0, maxfev=10000)
+        except:
+            pass
         angle[i] = Pa[2]
-        dia_a[i] = Pa[0]
-        dia_b[i] = Pa[1]
+        dia_a[i] = max(Pa[0],Pa[1])
+        dia_b[i] = min(Pa[0],Pa[1])
+        scale[i] = Pa[3]
+        sum_i[i] = np.sum(data_i)
 
         if rank == 0:
             print("CC (%d):"%i, ' sum_i = ', np.sum(data_i),  ' dia_a = ', dia_a[i], ' dia_b = ', dia_b[i], ' angle = ', angle[i])
@@ -136,19 +125,46 @@ def mp_worker(rank, indices, dia_a, dia_b, angle):
 
 nproc = 16
 ind = np.arange(n_frames)
-jobs = [mp.Process(target=mp_worker, args=(rank, ind, dia_a, dia_b, angle)) for rank in range(nproc)]
+jobs = [mp.Process(target=mp_worker, args=(rank, ind, dia_a, dia_b, angle, scale, sum_i)) for rank in range(nproc)]
 [i.start() for i in jobs]
 [i.join() for i in jobs]
 dia_a = np.frombuffer(dia_a.get_obj())
 dia_b = np.frombuffer(dia_b.get_obj())
 angle = np.frombuffer(angle.get_obj())
+scale = np.frombuffer(scale.get_obj())
+sum_i = np.frombuffer(sum_i.get_obj())
 ###############################################
-with h5py.File('diameter_weighted_error_mask0.h5', 'w') as f:
-    f['Run_num'] = running_num
-    f['diameter'] = dia
-    f['intens_tot'] = intens_tot
-    f['error'] = error
-    f['cc'] = cc
+with h5py.File('diameter_ab.h5', 'w') as f:
+    f['dia_a'] = dia_a
+    f['dia_b'] = dia_b
+    f['angle'] = angle
+    f['scale'] = scale
+    f['sum_i'] = sum_i
 
 
-np.savez('diameter_weighted_error_mask0.npz', dia=dia, intens=intens_tot, error=error, cc=cc)
+
+
+
+
+
+X = [flat_intrad, flat_floatang]
+intensity_map = intens(X, dia_a[0], dia_b[0], angle[0])
+plt.imshow(np.log10(intensity_map.reshape(512,256)))
+
+
+
+
+
+
+
+
+
+        f_i = emc.get_frame(i)
+        data_i = f_i.data[mask_center]
+        p0 = 17., 15., 0, 0.1
+        Pa, Pb = curve_fit(intens, X, data_i, p0)
+        plt.subplot(121)
+        plt.scatter(x1,y1,c=data_i,s=1)
+        mc = intens(X, Pa[0], Pa[1], Pa[2], Pa[3])
+        plt.subplot(122)
+        plt.scatter(x1,y1,c=mc,s=1)
